49db1a837c2b2dfeb073ed3510b39e8333b8f957
Brook Mesfin <brookmesfin101@gmail.com>
2020-01-17 08:46:54 -0600

    Cart Price Done, Working on Cart Updated Notification

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-    console.log($("select[name*='-gemQuantity']"));
+$(document).ready(function(){    
+    // Updates Total Price of Cart Items and Turns on cartUpdated Flag
+    $("select[name*='-gemQuantity']").each((i, v) => {
+        var el = $(v);
+
+        el.change(() => {
+            var name = $(el).attr("name").split("-gemQuantity")[0];
+            var quantity = $(el).children("option:selected").val();            
+            var price = $(`input[name='${name}-PerPrice'][type='hidden']`).val();
+
+            $("." + name + "-TotalPrice").text((price * quantity).toFixed(2));
+            if($("input[name='cartUpdated").val() === "false"){
+                $("input[name='cartUpdated").val("true").change();
+            }
+        })        
+    });
+        
+    $("input[name='cartUpdated").change((e) => {
+        $("#CartUpdatedNotification").toggle(".d-none");
+    })
 });
